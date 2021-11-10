dd4cb16d3009e7402aa77fb9013619c960561c34
collins <collinsboniface1000@gmail.com>
2021-11-10 18:56:13 +0200

    Adding the checkWinner method in the TicTacToe.java file

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,7 +1,9 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class TicTacToe {
+
+    static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
+    static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
 
     public static void main(String[] args) {
         char [] [] gameBoard = {    {' ', '|', ' ', '|', ' '},
@@ -27,6 +29,8 @@ public class TicTacToe {
             placePiece(gameBoard, cpuPos, "cpu");
 
             printGameBoard(gameBoard);
+
+            checkWinner();
         }
 
     }
@@ -89,6 +93,42 @@ public class TicTacToe {
             default:
                 break;
         }
+    }
+
+    public static String checkWinner() {
+
+        List topRow = Arrays.asList(1, 2, 3);
+        List midRow = Arrays.asList(4, 5, 6);
+        List botRow = Arrays.asList(7, 8, 9);
+
+        List leftCol = Arrays.asList(1, 4, 7);
+        List midCol = Arrays.asList(2, 5, 8);
+        List rightCol = Arrays.asList(3, 6, 9);
+
+        List cross1 = Arrays.asList(1, 5, 9);
+        List cross2 = Arrays.asList(7, 5, 3);
+
+        List<List> winning = new ArrayList<List>();
+        winning.add(topRow);
+        winning.add(midRow);
+        winning.add(botRow);
+        winning.add(leftCol);
+        winning.add(midCol);
+        winning.add(rightCol);
+        winning.add(cross1);
+        winning.add(cross2);
+
+        for (List l : winning) {
+            if(playerPositions.containsAll(l)){
+                return "Congratulations you won";
+            } else if (cpuPositions.contains(l)) {
+                return "CPU Wins! Sorry :(";
+            } else if(playerPositions.size() + cpuPositions.size() == 9) {
+                return "CAT!";
+            }
+        }
+
+        return "";
     }
 
 }
