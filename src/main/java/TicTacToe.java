7315bace44a0b3864f68973e806930e8e8013d25
collins <collinsboniface1000@gmail.com>
2021-11-10 18:19:28 +0200

    Updating the TicTacToe.java file by adding the function for the placePiece and moving the switch case in the method

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -18,43 +18,7 @@ public class TicTacToe {
 
         System.out.println(pos);
 
-        switch(pos) {
-            case 1:
-                gameBoard[0][0] = 'X';
-                break;
 
-            case 2:
-                gameBoard[0][2] = 'X';
-                break;
-
-            case 3:
-                gameBoard[0][4] = 'X';
-                break;
-
-            case 4:
-                gameBoard[2][0] = 'X';
-                break;
-
-            case 5:
-                gameBoard[2][2] = 'X';
-                break;
-
-            case 6:
-                gameBoard[2][4] = 'X';
-                break;
-
-            case 7:
-                gameBoard[4][0] = 'X';
-                break;
-
-            case 8:
-                gameBoard[4][2] = 'X';
-                break;
-
-            case 9:
-                gameBoard[4][4] = 'X';
-                break;
-        }
         printGameBoard(gameBoard);
 
     }
@@ -65,6 +29,57 @@ public class TicTacToe {
                 System.out.print(c);
             }
             System.out.println();
+        }
+    }
+
+    public static void placePiece(char[][] gameBoard, int pos, String user) {
+
+        char symbol = ' ';
+        if(user.equals("player")) {
+            symbol = 'X';
+        }   else if(user.equals("cpu")) {
+            symbol = 'O';
+        }
+
+        switch(pos) {
+            case 1:
+                gameBoard[0][0] = symbol;
+                break;
+
+            case 2:
+                gameBoard[0][2] = symbol;
+                break;
+
+            case 3:
+                gameBoard[0][4] = symbol;
+                break;
+
+            case 4:
+                gameBoard[2][0] = symbol;
+                break;
+
+            case 5:
+                gameBoard[2][2] = symbol;
+                break;
+
+            case 6:
+                gameBoard[2][4] = symbol;
+                break;
+
+            case 7:
+                gameBoard[4][0] = symbol;
+                break;
+
+            case 8:
+                gameBoard[4][2] = symbol;
+                break;
+
+            case 9:
+                gameBoard[4][4] = symbol;
+                break;
+
+            default:
+                break;
         }
     }
 
