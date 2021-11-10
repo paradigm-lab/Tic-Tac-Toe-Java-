a558412d7d9cad0f48a95372bbff53be9d4f2777
collins <collinsboniface1000@gmail.com>
2021-11-10 18:11:27 +0200

    Updating the TicTacToe.java file by adding the switch case that checkes the input of the user and add an X at the position

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -9,6 +9,7 @@ public class TicTacToe {
                                     {'-', '+', '-', '+', '-'},
                                     {' ', '|', ' ', '|', ' '}
                                 };
+
         printGameBoard(gameBoard);
 
         Scanner scan = new Scanner(System.in);
@@ -16,6 +17,45 @@ public class TicTacToe {
         int pos = scan.nextInt();
 
         System.out.println(pos);
+
+        switch(pos) {
+            case 1:
+                gameBoard[0][0] = 'X';
+                break;
+
+            case 2:
+                gameBoard[0][2] = 'X';
+                break;
+
+            case 3:
+                gameBoard[0][4] = 'X';
+                break;
+
+            case 4:
+                gameBoard[2][0] = 'X';
+                break;
+
+            case 5:
+                gameBoard[2][2] = 'X';
+                break;
+
+            case 6:
+                gameBoard[2][4] = 'X';
+                break;
+
+            case 7:
+                gameBoard[4][0] = 'X';
+                break;
+
+            case 8:
+                gameBoard[4][2] = 'X';
+                break;
+
+            case 9:
+                gameBoard[4][4] = 'X';
+                break;
+        }
+        printGameBoard(gameBoard);
 
     }
 
